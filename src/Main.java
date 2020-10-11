42a5f8f059ceca3af6de8f400789da6902083b1a
student <student@itpark.tech>
2020-10-11 08:37:14 +0300

    tv basic structure

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        TV tv = new TV();
+        System.out.println(tv.currentChannel);
+        System.out.println(tv.currentVolume); // 0, 0.0, false, null
+    }
+}
---
+++ src/TV.java
@@ -0,0 +1,22 @@
+public class TV {
+    int currentChannel;
+    int currentVolume;
+
+    int increaseVolume() {
+        return 0;
+    }
+    int decreaseVolume() {
+        return 0;
+    }
+    // Ctrl + Shift + Alt + левая кнопка мыши (multi-cursors)
+    // Escape - выход из режима multi-cursor
+    int increaseChannel() {
+        return 0;
+    }
+    int decreaseChannel() {
+        return 0;
+    }
+    int setChannel(int channel) {
+        return 0;
+    }
+}
